fc34c48522761f561dd42b1f87ab07472a870de5
Mariusz Macheta <Mariusz.Macheta@gmail.com>
2024-01-07 03:04:54 +0100

    Day 14 part 2

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -1,62 +1,58 @@
-﻿var input = File.ReadAllLines("input.txt")
+﻿using System.Collections.Immutable;
+
+var input = File.ReadAllLines("input.txt")
 	.ToList();
 
-var template = input.First().AsEnumerable();
+var template = input.First();
 
 var insertions = input
 	.Skip(2)
 	.Select(i => i.Split(" -> "))
-	.Select(i => new Insertion(i[0][0], i[0][1], i[1][0]))
-	.ToList();
+	.ToDictionary(i => i[0], i => i[1][0]);
 
-var prevStats = new List<Stat>();
 
-for (int i = 0; i < 20; i++)
+var result1 = template.GroupBy(c => c).ToImmutableDictionary(t => t.Key, t => (long)t.Count());
+var result2 = template.GroupBy(c => c).ToImmutableDictionary(t => t.Key, t => (long)t.Count());
+var cache = new Dictionary<(char, char, int), ImmutableDictionary<char, long>>();
+
+for (int i = 0; i < template.Length - 1; i++)
 {
-	Console.WriteLine("Iteration " + i);
+	result1 = CombineDictionaries(result1, GetElementCounts(template[i], template[i + 1], 10, insertions));
+	result2 = CombineDictionaries(result2, GetElementCounts(template[i], template[i + 1], 40, insertions));
+}
 
-	template = PerformInsertions(template, insertions).ToList();
+Console.WriteLine("1: " + (result1.Select(r => r.Value).Max() - result1.Select(r => r.Value).Min()));
+Console.WriteLine("2: " + (result2.Select(r => r.Value).Max() - result2.Select(r => r.Value).Min()));
 
-	var stats = template.GroupBy(t => t)
-		.Select(x => new Stat(x.Key, x.LongCount()))
-		.OrderByDescending(x => x.count)
-		.ToList();
+ImmutableDictionary<char, long> GetElementCounts(char first, char second, int iterations, Dictionary<string, char> insertions)
+{
+	var key = (first, second, iterations);
+	if (cache.TryGetValue(key, out var cacheResult)) return cacheResult;
 
-	foreach (var stat in stats)
+	var insertion = insertions[first.ToString() + second];
+	var iteratoionValue = ImmutableDictionary<char, long>.Empty.Add(insertion, 1);
+	if (iterations == 1)
 	{
-		var prevStat = prevStats.FirstOrDefault(s => s.c == stat.c);
-		Console.WriteLine($"Char {stat.c} Count: {stat.count} Diff: {stat.count - (prevStat?.count ?? stat.count)}");
+		return iteratoionValue;
 	}
 
-	Console.WriteLine($"Result: {GetResult(stats)} Diff: {GetResult(stats) - GetResult(prevStats)}");
-	prevStats = stats;
-	Console.WriteLine();
+	var leftResult = GetElementCounts(first, insertion, iterations - 1, insertions);
+	var rightResult = GetElementCounts(insertion, second, iterations - 1, insertions);
+
+	var result = CombineDictionaries(iteratoionValue, CombineDictionaries(leftResult, rightResult));
+	cache[key] = result;
+	return result;
 }
 
-IEnumerable<char> PerformInsertions(IEnumerable<char> template, List<Insertion> insertions)
+static ImmutableDictionary<char, long> CombineDictionaries(ImmutableDictionary<char, long> leftResult, ImmutableDictionary<char, long> rightResult)
 {
-	var first = template.First();
-	var second = first;
-	yield return first;
-
-	foreach (var c in template.Skip(1))
+	var result = ImmutableDictionary.CreateBuilder<char, long>();
+	foreach (var key in leftResult.Keys.Union(rightResult.Keys))
 	{
-		second = c;
-		var insertion = insertions.FirstOrDefault(i => i.first == first && i.second == second);
-		if (insertion != null)
-		{
-			yield return insertion.toInsert;
-		}
-
-		first = second;
-		yield return second;
+		leftResult.TryGetValue(key, out long left);
+		rightResult.TryGetValue(key, out long right);
+		result.Add(key, left + right);
 	}
-}
 
-long GetResult(List<Stat> stats)
-{
-	return stats.Any() ? stats.First().count - stats.Last().count : 0;
+	return result.ToImmutable();
 }
-
-record Insertion(char first, char second, char toInsert);
-record Stat(char c, long count);
